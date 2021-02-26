686577cd5b05459ab5f78b5e351b87c6937d9fd2
TanyaChichkan <tchichkan28@gmail.com>
2021-02-26 17:28:40 +0200

    updated

--- src/js/menuNav.js
+++ src/js/menuNav.js
@@ -3,38 +3,64 @@ import {refs} from './refs';
 
 
 if(refs.container.style.width<1440){
-    console.log(refs.container.style.width);
-    refs.navList.addEventListener('click',navHandler);
+    refs.menuWrapper.addEventListener('click',navHandler);
 } 
 
-const arr = ["clothing", "collections", "collabs", "brands" ]
+const arr = ["clothing", "collections", "collabs", "brands" ];
+const navLinks = ["What's new", "women", "accessories", "kids", "beauty", "outlet", "stories" ];
+const miniCategeory = ["T-shirts","Sweatshirts","Knitwear","Jeans","Jumpsuits","Skirts","Dresses"];
 
 function navHandler(e){
-    console.log(refs.container.style.width);
-    refs.navListItem.forEach((item,index,array)=>{
-        if (Number(e.target.dataset.index) === 1) {
-
+    console.log(e.target);
+    refs.navListItem.forEach((item,index)=>{
+      
+        if ((Number(e.target.dataset.index) === 1) && refs.navList.dataset.id==="first-page") {
             refs.categoryBlock.classList.toggle('is-rendered');
             refs.categoryBlock.children[0].textContent = e.target.firstElementChild.textContent;
-            refs.navList.innerHTML=markUpRender();
+            refs.navList.dataset.id='second-page';
+            refs.navList.innerHTML=markUpRender(arr,2);
         }
-    })
-    
+
+        if((Number(e.target.dataset.index) === 0) && refs.navList.dataset.id==="second-page"){
+            refs.navList.innerHTML=markUpRender(miniCategeory,3);
+            refs.categoryBlock.children[0].textContent = e.target.firstElementChild.textContent;
+        }
+    });
+
+    if(e.target.classList.contains('history-link')){
+        console.log(e.target);
+        refs.categoryBlock.classList.toggle('is-rendered');
+        refs.navList.innerHTML=markUpRender(navLinks,1);
+        refs.navList.dataset.id="first-page";
+
+    }  
 };
 
 
-function markUpRender(){
-    const markup = arr.map((el,index)=>{
-    return `<li class="navigation-list-item" data-index=${String(index)}>
-        <div class="list-wrapper">
-        ${el}
-        <svg class="icon-right">
-        <use href="./images/symbol-defs.svg#icon-right"></use>
-        </svg>
-        </div>
-        </li>`
-    });
+function markUpRender(array, pageNumber){
+    let markup;
 
-    return markup;
+    if(pageNumber !== 3){
+        markup = array.map((el,index)=>{
+            return `<li class="navigation-list-item" data-index=${String(index)}>
+            <div class="list-wrapper">
+            ${el}
+            <svg class="icon-right">
+            <use href="./images/symbol-defs.svg#icon-right"></use>
+            </svg>
+            </div>
+            </li>`
+        });
+    } else {
+        markup = array.map((el,index)=>{
+            return `<li class="navigation-list-item" data-index=${String(index)}>
+            <div class="list-wrapper">
+            ${el}
+           </div>
+            </li>`
+        });
+    }
+
+    return markup.join("");
         
 }
