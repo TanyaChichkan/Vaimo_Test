9819ba60750848c3c8d9eb7be7cf05babbdabc98
TanyaChichkan <tchichkan28@gmail.com>
2021-02-26 18:53:09 +0200

    upd

--- src/js/menuNav.js
+++ src/js/menuNav.js
@@ -16,24 +16,43 @@ function navHandler(e){
       
         if ((Number(e.target.dataset.index) === 1) && refs.navList.dataset.id==="first-page") {
             refs.categoryBlock.classList.toggle('is-rendered');
-            refs.categoryBlock.children[0].textContent = e.target.firstElementChild.textContent;
+            refs.categoryBlock.children[1].textContent = e.target.firstElementChild.textContent;
             refs.navList.dataset.id='second-page';
             refs.navList.innerHTML=markUpRender(arr,2);
         }
 
         if((Number(e.target.dataset.index) === 0) && refs.navList.dataset.id==="second-page"){
             refs.navList.innerHTML=markUpRender(miniCategeory,3);
-            refs.categoryBlock.children[0].textContent = e.target.firstElementChild.textContent;
+            refs.categoryBlock.children[1].textContent = e.target.firstElementChild.textContent;
+            refs.iconGoBack.style.display="block";
+            refs.navList.dataset.id='third-page';
         }
     });
 
     if(e.target.classList.contains('history-link')){
-        console.log(e.target);
         refs.categoryBlock.classList.toggle('is-rendered');
         refs.navList.innerHTML=markUpRender(navLinks,1);
         refs.navList.dataset.id="first-page";
-
     }  
+
+    if(e.target.dataset.id==="go-back" && refs.navList.dataset.id==="third-page"){
+        refs.categoryBlock.children[1].textContent = e.target.firstElementChild.textContent;
+        refs.navList.dataset.id='second-page';
+        refs.navList.innerHTML=markUpRender(arr,2);
+    }
+
+    if(e.target.dataset.id==="go-back" && refs.navList.dataset.id==="third-page"){
+        refs.categoryBlock.children[1].textContent = e.target.firstElementChild.textContent;
+        refs.navList.dataset.id='second-page';
+        refs.navList.innerHTML=markUpRender(arr,2);
+    }
+
+    if(e.target.dataset.id==="go-back" && refs.navList.dataset.id==="second-page"){
+        refs.categoryBlock.classList.toggle('is-rendered');
+        refs.navList.innerHTML=markUpRender(navLinks,1);
+        refs.navList.dataset.id="first-page";
+    }
+
 };
 
 
--- src/js/refs.js
+++ src/js/refs.js
@@ -14,5 +14,6 @@ export const refs={
     navList:document.querySelector('.navigation-list'),
     navListItem: Array.from(document.querySelectorAll('.navigation-list-item')),
     categoryBlock: document.querySelector('.category-block'),
+    iconGoBack:document.querySelector('.menu-wrapper .icon-right'),
 
 }
